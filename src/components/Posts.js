da4e7a37de68b69b0aaf9b5e14d8e379e1329c67
Julia Robinson <juliaarobinson@hotmail.com>
2022-03-31 21:31:45 +0100

    get posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,3 +1,18 @@
-const Posts = () => <h1>Hello</h1>;
+import useAxios from "../hooks/use-axios";
+
+const Posts = () => {
+  const { response, loading, error } = useAxios({
+    method: "get",
+    url: "https://jsonplaceholder.typicode.com/posts",
+  });
+
+  return (
+    <>
+      {loading && <p>Loading</p>}
+      {response && response.map((post) => post.title)}
+      {error && <p>{error.message}</p>}
+    </>
+  );
+};
 
 export default Posts;
--- src/hooks/use-axios.js
+++ src/hooks/use-axios.js
@@ -1,5 +1,27 @@
-const useAxios = () => {
-  return "hello";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const useAxios = ({ method, url }) => {
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(true);
+  const [response, setResponse] = useState();
+
+  useEffect(() => {
+    const makeRequest = () => {
+      axios[method](url)
+        .then((response) => {
+          setResponse(response.data);
+        })
+        .catch((error) => {
+          setError(error);
+        })
+        .finally(() => setLoading(false));
+    };
+
+    makeRequest();
+  }, [url, method]);
+
+  return { response, error, loading };
 };
 
 export default useAxios;
